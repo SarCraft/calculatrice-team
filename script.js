7e70363446c217cd056539441512fd443e0ddb08
Sonny Naidja <sonny.naidja@protonmail.com>
2025-10-16 12:29:57 +0200

    add multiplication and base html

---
+++ script.js
@@ -0,0 +1,17 @@
+function multiplier() {
+      // Récupération des valeurs des champs input
+      var a = parseFloat(document.getElementById("valeur1").value);
+      var b = parseFloat(document.getElementById("valeur2").value);
+
+      // Vérification si les valeurs sont bien des nombres
+      if (isNaN(a) || isNaN(b)) {
+        alert("Veuillez entrer deux nombres valides.");
+        return;
+      }
+
+      // Calcul de la multiplication
+      var resultat = a * b;
+
+      // Affichage du résultat dans la page
+      document.getElementById("resultat").innerText = "Résultat : " + resultat;
+    }
